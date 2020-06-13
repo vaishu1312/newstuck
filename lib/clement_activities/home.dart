b2ddca61cc8cf36c1c56f4b48249bd90101ee6b4
rclemsmith <rclemsmith@gmail.com>
2020-06-13 20:09:52 +0530

    Second push

--- lib/clement_activities/home.dart
+++ lib/clement_activities/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:newstuck/clement_activities/home.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    home: Home(),
   ));
 }
 
