6df4b74f5337de3c54c3d89cabfedf0b629c5704
rclemsmith <rclemsmith@gmail.com>
2020-06-15 14:56:30 +0530

    Ranks and Tags

---
+++ lib/clement_activities/ranks.dart
@@ -0,0 +1,149 @@
+import 'package:dropdownfield/dropdownfield.dart';
+import 'package:flutter/material.dart';
+
+Widget rankBar() {
+  String competency = "None";
+  List<String> competencyList = [
+    "None",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
+  dynamic trustworthiness = "None";
+  String factsvsopinion = "None";
+
+  return Scaffold(
+    body: Wrap(children: [
+      Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              child: Text('Competency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: new Color(0xff9a2424),
+                      fontFamily: 'Lobster')),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
+              decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(30)),
+
+              // dropdown below..
+              child: DropdownButton<String>(
+                  value: competency,
+                  icon: Icon(Icons.arrow_drop_down),
+                  iconSize: 42,
+                  underline: SizedBox(),
+                  onChanged: (String newValue) {
+                    competency = newValue;
+                    print(competency);
+                  },
+                  items: competencyList
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList()),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              child: Text('TrustWorthiness',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: new Color(0xff9a2424),
+                      fontFamily: 'Lobster')),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
+              decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(30)),
+
+              // dropdown below..
+              child: DropdownButton<String>(
+                  value: competency,
+                  icon: Icon(Icons.arrow_drop_down),
+                  iconSize: 42,
+                  underline: SizedBox(),
+                  onChanged: (String newValue) {
+                    trustworthiness = newValue;
+                    // print(competency);
+                  },
+                  items: competencyList
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList()),
+            ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              child: Text('FactVsOpinion',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: new Color(0xff9a2424),
+                      fontFamily: 'Lobster')),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 1),
+              decoration: BoxDecoration(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(30)),
+
+              // dropdown below..
+              child: DropdownButton<String>(
+                  value: competency,
+                  icon: Icon(Icons.arrow_drop_down),
+                  iconSize: 42,
+                  underline: SizedBox(),
+                  onChanged: (String newValue) {
+                    factsvsopinion = newValue;
+                    // print(competency);
+                  },
+                  items: competencyList
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList()),
+            ),
+          ],
+        ),
+      ),
+    ]),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:newstuck/clement_activities/home.dart';
+import 'package:newstuck/clement_activities/ranks.dart';
+import 'package:newstuck/clement_activities/tags.dart';
 import 'package:newstuck/vaishali/dashboard.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     //home: Home(),
-    home:Home(),
+    home:TagBuild(),
   ));
 }
 
