eb651b9ea231f9865a38672fe4f84ef4b5f03d9a
rclemsmith <rclemsmith@gmail.com>
2020-06-16 21:18:35 +0530

    Get Feed Items API

--- lib/vaishali/headline.dart
+++ lib/vaishali/headline.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class HeaderRow extends StatefulWidget {
+  var feed = new Map<String, dynamic>();
+  HeaderRow(this.feed);
   @override
   State<StatefulWidget> createState() => HeaderRowState();
 }
@@ -9,20 +11,36 @@ class HeaderRow extends StatefulWidget {
 class HeaderRowState extends State<HeaderRow> {
   bool isPressed = false;
 
+  var feedItem = new Map<String, dynamic>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    feedItem = widget.feed;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: MyLink('HeadLines go here'),
+        title: MyLink(feedItem["title"]),
         subtitle: Text('Mon Jun 15 2020 09:39:37  | The Hindu - News |'),
         trailing: ClipOval(
           child: Material(
-            color: (isPressed) ? Color(0xff00AA00) : Color(0xff9A9A9A), // button color
+            color: (isPressed)
+                ? Color(0xff00AA00)
+                : Color(0xff9A9A9A), // button color
             child: InkWell(
-              splashColor: (isPressed) ? Color(0x6600AA00) : Color(0x669A9A9A), // inkwell color
-              child: SizedBox(width: 56, height: 56, child: Icon(Icons.done_outline)),
-              onTap: () {setState(() {
-              isPressed = !isPressed;
-            });},
+              splashColor: (isPressed)
+                  ? Color(0x6600AA00)
+                  : Color(0x669A9A9A), // inkwell color
+              child: SizedBox(
+                  width: 56, height: 56, child: Icon(Icons.done_outline)),
+              onTap: () {
+                setState(() {
+                  isPressed = !isPressed;
+                });
+              },
             ),
           ),
         )
