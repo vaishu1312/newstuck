704c791d2b922bdd5097f34a125936f4b4a1bc1a
rclemsmith <rclemsmith@gmail.com>
2020-06-19 20:54:03 +0530

    Not Final

--- lib/clement_activities/const.dart
+++ lib/clement_activities/const.dart
@@ -2,7 +2,8 @@
 
 String returnDomain(){
 
-return "http://localhost:5000/";
-//  return "http://newstuck.southindia.cloudapp.azure.com/" ;
+// return "http://localhost:5000/";
+ return "http://newstuck.southindia.cloudapp.azure.com/" ;
+// return "https://flutterbackend.azurewebsites.net/";
 
 }
