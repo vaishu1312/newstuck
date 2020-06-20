8f5d0ff6e44d78abfbf72d6f408f4d0aa932cad3
rclemsmith <rclemsmith@gmail.com>
2020-06-20 21:01:52 +0530

    Added Internet Permission

--- lib/vaishali/dashboard.dart
+++ lib/vaishali/dashboard.dart
@@ -230,60 +230,60 @@ class MyDashBoardState extends State<MyDashBoard> {
     return Scaffold(
       appBar: CustomAppBar(),
       body: Container(
-        child: Column(children: [
-          Container(
-              height: h * 0.1,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey[300],
-                    blurRadius: 4,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                    MyToggle(onToggleSelected),
-                    Text('Selected Items Only',
-                        style: TextStyle(color: Color(0xAA000000))),
-                  ]),
-                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
-                    CustomDropdown(dropFilter, isToggleSelected, setText)
-                  ]),
-                ],
-              )),
-          Expanded(
-            child: ListView.builder(
-              controller: _scrollController,
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: load ? feedItems.length + 1 : feedItems.length,
-              itemBuilder: (context, index) {
-                if (index == feedItems.length && load) {
-                  return CupertinoActivityIndicator(
-                    radius: 20,
-                  );
-                }
-
-                return Container(
-                  //height: h - CustomAppBar().preferredSize.height - (h * 0.15),
-                  padding: EdgeInsets.all(10.0),
-                  color: Color(0xFF9a2424),
-                  child: Card(
-                    //child: ExpandRow(),
-                    child: HeaderRow(feedItems[index],isToggleSelected),
-                  ),
-                );
-              },
+          child: Column(children: [
+            Container(
+        height: h * 0.1,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[300],
+              blurRadius: 4,
+              offset: Offset(0, 3),
             ),
-          ),
-        ]),
+          ],
+        ),
+        padding: EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              MyToggle(onToggleSelected),
+              Text('Selected Items Only',
+                  style: TextStyle(color: Color(0xAA000000))),
+            ]),
+            Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              CustomDropdown(dropFilter, isToggleSelected, setText)
+            ]),
+          ],
+        )),
+            Expanded(
+      child: ListView.builder(
+        controller: _scrollController,
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: load ? feedItems.length + 1 : feedItems.length,
+        itemBuilder: (context, index) {
+          if (index == feedItems.length && load) {
+            return CupertinoActivityIndicator(
+              radius: 20,
+            );
+          }
+
+          return Container(
+            //height: h - CustomAppBar().preferredSize.height - (h * 0.15),
+            padding: EdgeInsets.all(10.0),
+            color: Color(0xFF9a2424),
+            child: Card(
+              //child: ExpandRow(),
+              child: HeaderRow(feedItems[index],isToggleSelected),
+            ),
+          );
+        },
       ),
+            ),
+          ]),
+        ),
     );
   }
 
