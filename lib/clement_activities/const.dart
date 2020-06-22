a92e4e46ab4919366f4390fae04754548049ad4e
rclemsmith <rclemsmith@gmail.com>
2020-06-22 21:42:14 +0530

    Committ

--- lib/clement_activities/const.dart
+++ lib/clement_activities/const.dart
@@ -7,6 +7,7 @@ String returnDomain(){
 //return "http://localhost:60395/";
   return "http://newstuck.southindia.cloudapp.azure.com/" ; 
 // return "https://flutterbackend.azurewebsites.net/";
+// return "https://newstuckapi3.azurewebsites.net/";
 
 
 }
